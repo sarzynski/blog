8f5bc82ab7cc007277b6728c3a12a4d624aa89d6
Kamil Sarzyński <kamil.s.sarzynski@gmail.com>
2021-05-11 09:11:48 +0200

    add user to post

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
-  devise_for :users
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   root 'posts#index'
 end
